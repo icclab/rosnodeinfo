9398946f633163679fd84a34341ab9d1c3b855f0
turtlebot <rodrigue.de.schaetzen@hotmail.com>
2019-04-02 15:29:09 +0200

    gets rid of static topic name

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
 # rosnodeinfo
 
-ROS node that publishes to a topic all of the currently running nodes.
+ROS node that publishes to a topic all of the currently running nodes. By default the name of the published topic is `running_nodes` and the node name is `nodeInfoPublisher`.
 
 ```
 rosrun rosnodeinfo nodes.py
 ```
+
+For a different topic name and/or node name
+
+```
+rosrun rosnodeinfo nodes.py _topic:='example_topic_name' __name:='example_node_name'
+```
+
 
--- src/nodes.py
+++ src/nodes.py
@@ -7,8 +7,10 @@ import rosnode
 from std_msgs.msg import String
 
 def pubNodeInfos():
-  pub = rospy.Publisher('runningNodes', String, queue_size=1)
-  rospy.init_node('nodeInfoPublisher', anonymous=False)
+  # Get the ~private namespace parameters from command line or launch file.
+  topic = rospy.get_param('~topic', 'running_nodes')
+  rospy.loginfo('topic = %s', topic)
+  pub = rospy.Publisher(topic, String, queue_size=1)
   
   while not rospy.is_shutdown():
     nodeString = ""
@@ -23,7 +25,8 @@ def pubNodeInfos():
 
 
 if __name__ == '__main__':
-  try:  
+  try:
+    rospy.init_node('nodeInfoPublisher', anonymous=False)
     pubNodeInfos()
   except rospy.ROSInterruptException:
     print "node interrupted"
